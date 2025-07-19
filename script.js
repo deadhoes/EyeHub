adab5d787f0586bb2f6a456ede4db7b3f0843ada
EyeUI <noctalis.softworks@gmail.com>
2025-07-20 00:43:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-document.addEventListener("contextmenu", (e) => e.preventDefault());
-
-document.getElementById("discordBtn").addEventListener("click", () => {
-  window.open("https://discord.gg/9krxH9g4MH", "_blank");
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
 });
 
-document.getElementById("githubBtn").addEventListener("click", () => {
-  window.open("https://github.com/deadhoes/EyeHub", "_blank");
-});
-
-const toggle = document.getElementById("modeToggle");
-const body = document.body;
-
-toggle.addEventListener("change", () => {
-  if (toggle.checked) {
-    body.classList.remove("light-mode");
-    body.classList.add("dark-mode");
-  } else {
-    body.classList.remove("dark-mode");
-    body.classList.add("light-mode");
-  }
+document.querySelectorAll('a[href^="http"]').forEach(link => {
+  link.setAttribute('rel', 'noopener noreferrer');
 });
