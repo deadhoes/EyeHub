92a3f3e728c65bb933253abd7f709fc143b2c735
EyeUI <noctalis.softworks@gmail.com>
2025-07-19 20:21:22 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("contextmenu", (e) => e.preventDefault());
+
+document.getElementById("discordBtn").addEventListener("click", () => {
+  window.open("https://discord.gg/9krxH9g4MH", "_blank");
+});
+
+document.getElementById("githubBtn").addEventListener("click", () => {
+  window.open("https://github.com/deadhoes/EyeHub", "_blank");
+});
